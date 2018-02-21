81fd290fb914f58dbf83e036131fa6a3c2b5881a
Bhaumik <bhaumik@Bhaumiks-MacBook-Pro.local>
2018-02-21 12:41:23 +0100

    initial working version

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ const train = require('.');
 const learning_rate = 300;
 const layers = [2, 3, 2];
 const input = [1, 0];
-const output = [1, 0];
+const output = [0, 1];
 const iterations = 2e4;
 console.log(train(learning_rate, input, layers, output, iterations));
